fd2789508a99af1f035fc80f8081dd1e7b61c6e4
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2018-03-30 19:54:01 +1100

    updates

---
+++ contentscript.js
@@ -0,0 +1,8 @@
+/**
+ * Content scripts execute in the context of a page that's been loaded into the 
+ * browser. 
+ * - Think of a content script as part of a loaded page, not the extension
+ * - Content scripts can read & modify details of  web pages the browser visits
+ * - Cannot modify the DOM of its parent extension's background page.
+*/
+
