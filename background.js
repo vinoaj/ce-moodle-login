849477cc89413f9120462e9391a4a4ed7fdaaa58
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2018-03-30 19:54:19 +1100

    updates

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
+// background.js holds the main logic of the extension
+// Persistent pages are always open (not recommended)
+// Event pages opened & closed as needed
+
+// If login link is present then the user is not logged in
 var ruleNotLoggedIn = {
     conditions: [
         new chrome.declarativeContent.PageStateMatcher({
@@ -5,7 +10,13 @@ var ruleNotLoggedIn = {
                 hostPrefix: 'moodle.',
                 schemes: ['https']
             },
-            css: [".login a"]
+            css: ["a[href*='/login/index.php']"]
         })
     ]
 };
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([ruleNotLoggedIn]);
+    });
+});
