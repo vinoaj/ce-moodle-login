b60225a293a5db00d0aae5c004ef66245a34cf7e
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2018-03-31 19:29:11 +1100

    cleanup

--- contentscript.js
+++ contentscript.js
@@ -6,31 +6,28 @@
  * - Cannot modify the DOM of its parent extension's background page.
 */
 
-//alert('x');
-console.log('CHREX2: ContentScript.js');
-console.log(document);
-var port = chrome.runtime.connect();
+// var port = chrome.runtime.connect();
 
 function navigateToLoginPage() {
-    var el = document.querySelectorAll("a[href*='/login/index.php']")[0];
-    el.click();
+    document.location = "https://ssologin.unsw.edu.au/cas/login?service=https://moodle.telt.unsw.edu.au/login/index.php?authCAS=CAS";
+    // var el = document.querySelectorAll("a[href*='/login/index.php']")[0];
+    // el.click();
 }
 
 function enterUsername(){
-    var el = document.getElementById('username');
-    // el.value = chrome.storage.sync.get({"username": ""});
+    chrome.storage.sync.get({"username": ""}, function(items) {
+        document.getElementById('username').value = items.username;
+    });
 }
 
 function enterPassword(){
-    var el = document.getElementById('password');
-    // el.value = chrome.storage.sync.get({"password": ""});
+    chrome.storage.sync.get({"password": ""}, function(items) {
+        document.getElementById('password').value = items.password;
+    });
 }
 
 function submit() {
-    console.log("In submit");
-    var el = document.querySelector(".visible-xs input#submit");
-    console.log(el);
-    el.click();
+    document.querySelector(".visible-xs input#submit").click();
 }
 
 function init() {
@@ -40,12 +37,10 @@ function init() {
         console.log("In else loop");
         enterUsername();
         enterPassword();
-        submit();
+        window.setTimeout(submit,1000);
     }
 }
 
-document.addEventListener('DOMContentLoaded', init);
-console.log(document.readyState);
 if (document.readyState == 'interactive') {
     init();
 }
